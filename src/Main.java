b1d56ffb807c88dce7a49981458deabfd2aed745
Coralnws <ngwengshan025@hotmail.com>
2022-05-27 20:12:35 +0800

    round corner

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.ImagePattern;
 import javafx.scene.paint.Paint;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
@@ -17,10 +20,11 @@ import javafx.stage.StageStyle;
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(loader.getClassLoader().getResource("fxml/main.fxml"));
-        Parent root = loader.load();
 
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(loader.getClassLoader().getResource("fxml/main2.fxml"));
+        Parent root = loader.load();
+        root.setStyle("-fx-background-image:url('file:/C:/Users/USER/Desktop/北航/面向对象Java/Y3/src/pic/bgsetround.png');-fx-background-size:cover;-fx-background-radius: 20;");
         Scene scene = new Scene(root);
         scene.setFill(Paint.valueOf("#ffffff00"));
 
