3d516c6ab6cd03194af07d95b9de5ccf40715c19
PikaTea <pqy138@gmail.com>
2022-06-15 17:46:05 +0800

    Notes done

--- src/Notes.java
+++ src/Notes.java
@@ -1,7 +1,6 @@
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
@@ -11,14 +10,15 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.Region;
 
+import java.io.*;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 public class Notes {
     Parent root;
     GridPane notePane;
     Button add;
+    List<String> saveIdList = new ArrayList<>(10);
     List<TextArea> taList = new ArrayList<>(10);
 
     int curNote = 0;
@@ -57,17 +57,17 @@ public class Notes {
 
         Button love = new Button("♥");
         Button delete = new Button("X");
-        Button smile = new Button("☺");
+        Button balloon = new Button("\uD83C\uDF88");
         Button save = new Button("保存");
         Region filler = new Region();
         love.setStyle(String.valueOf(buttonStyle));
         delete.setStyle(String.valueOf(buttonStyle));
-        smile.setStyle(String.valueOf(buttonStyle));
+        balloon.setStyle(String.valueOf(buttonStyle));
         filler.setStyle(String.valueOf(buttonStyle));
         save.setStyle(String.valueOf(buttonStyle));
         love.setId("love" + curNote);
         delete.setId("delete" + curNote);
-        smile.setId("smile" + curNote);
+        balloon.setId("balloon" + curNote);
         save.setId("save" + curNote);
 
         EventHandler<MouseEvent> clickLove = new EventHandler<MouseEvent>() {
@@ -80,18 +80,13 @@ public class Notes {
             }
         };
 
-        EventHandler<MouseEvent> clickDelete = new EventHandler<MouseEvent>() {
+        EventHandler<MouseEvent> clickBalloon = new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent e) {
-                System.out.println("CLICK " + delete.getId());
-                deleteNote(delete.getId());
-            }
-        };
-
-        EventHandler<MouseEvent> clickSmile = new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent e) {
-                System.out.println("CLICK " + smile.getId());
+                System.out.println("CLICK " + balloon.getId());
+                Integer curNote = Integer.valueOf(balloon.getId().substring(7,8));
+                TextArea tempNote = taList.get(curNote);
+                tempNote.appendText("\uD83C\uDF88");
             }
         };
 
@@ -99,23 +94,23 @@ public class Notes {
             @Override
             public void handle(MouseEvent e) {
                 System.out.println("CLICK " + save.getId());
-                saveNote();
+                saveNote(save.getId());
             }
         };
 
         love.addEventHandler(MouseEvent.MOUSE_CLICKED, clickLove);
-        delete.addEventHandler(MouseEvent.MOUSE_CLICKED, clickDelete);
-        smile.addEventHandler(MouseEvent.MOUSE_CLICKED, clickSmile);
+        balloon.addEventHandler(MouseEvent.MOUSE_CLICKED, clickBalloon);
         save.addEventHandler(MouseEvent.MOUSE_CLICKED, clickSave);
 
         HBox.setHgrow(filler, Priority.ALWAYS);
         HBox controlsContainer = new HBox();
-        controlsContainer.getChildren().addAll(love, smile, save, filler, delete);
+        controlsContainer.getChildren().addAll(love, balloon, save, filler, delete);
 
         TextArea tempNote = new TextArea();
         tempNote.setPromptText("Write your text here!");
         tempNote.setWrapText(true);
         tempNote.setStyle(String.valueOf(noteStyle));
+        saveIdList.add(save.getId());
         taList.add(tempNote);
         GridPane.setMargin(tempNote, new Insets(0, 0, 10, 0));
 
@@ -127,19 +122,200 @@ public class Notes {
         }
         curNote += 1;
 
+        EventHandler<MouseEvent> clickDelete = new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent e) {
+                System.out.println("CLICK " + delete.getId());
+                deleteNote(save.getId());
+                notePane.getChildren().remove(tempNote);
+                notePane.getChildren().remove(controlsContainer);
+
+                curNote = 0;
+                gridPaneX = 0;
+                gridPaneY = 0;
+
+                saveIdList.removeAll(saveIdList);
+                taList.removeAll(taList);
+                loadNote();
+
+            }
+        };
+        delete.addEventHandler(MouseEvent.MOUSE_CLICKED, clickDelete);
 
     }
 
-    private void deleteNote(String deleteId) {
-        System.out.println(deleteId.length());
+    private void deleteNote(String saveId) {
+        System.out.println("DELETE note: " + saveId);
+
+        File delFile = new File("D:\\IDEA Project\\Y3\\src\\notes\\" + saveId + ".txt");
+        if(delFile.delete()) {
+            System.out.println("DELETE SUCCESS");
+        }
+        else {
+            System.out.println("DELETE FAILED");
+        }
+
+        Integer delSaveId = Integer.valueOf(saveId.substring(4,5));
+
+        for(int i = delSaveId; i < curNote - 1; i++) {
+            File file = new File("D:\\IDEA Project\\Y3\\src\\notes\\save" + (i+1) + ".txt");
+            File rename = new File("D:\\IDEA Project\\Y3\\src\\notes\\save" + i + ".txt");
+
+            if(file.renameTo(rename)) {
+                System.out.println("RENAME SUCCESS");
+            }
+            else {
+                System.out.println("RENAME FAILED");
+            }
+        }
+        
     }
 
     private void loadNote() {
+        notePane.getChildren().clear();
+        File directory = new File("D:\\IDEA Project\\Y3\\src\\notes\\");
+        int fileCount = directory.list().length;
+        System.out.println("Load File Count:"+fileCount);
+
+        for(int i = 0; i < fileCount; i++) {
+            Button love = new Button("♥");
+            Button delete = new Button("X");
+            Button balloon = new Button("\uD83C\uDF88");
+            Button save = new Button("保存");
+            Region filler = new Region();
+            love.setStyle(String.valueOf(buttonStyle));
+            delete.setStyle(String.valueOf(buttonStyle));
+            balloon.setStyle(String.valueOf(buttonStyle));
+            filler.setStyle(String.valueOf(buttonStyle));
+            save.setStyle(String.valueOf(buttonStyle));
+            love.setId("love" + curNote);
+            delete.setId("delete" + curNote);
+            balloon.setId("balloon" + curNote);
+            save.setId("save" + curNote);
+
+            EventHandler<MouseEvent> clickLove = new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent e) {
+                    System.out.println("CLICK " + love.getId());
+                    Integer curNote = Integer.valueOf(love.getId().substring(4, 5));
+                    TextArea tempNote = taList.get(curNote);
+                    tempNote.appendText("♥");
+                }
+            };
+
+            EventHandler<MouseEvent> clickBalloon = new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent e) {
+                    System.out.println("CLICK " + balloon.getId());
+                    Integer curNote = Integer.valueOf(balloon.getId().substring(7, 8));
+                    TextArea tempNote = taList.get(curNote);
+                    tempNote.appendText("\uD83C\uDF88");
+                }
+            };
+
+            EventHandler<MouseEvent> clickSave = new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent e) {
+                    System.out.println("CLICK " + save.getId());
+                    saveNote(save.getId());
+                }
+            };
+
+            love.addEventHandler(MouseEvent.MOUSE_CLICKED, clickLove);
+            balloon.addEventHandler(MouseEvent.MOUSE_CLICKED, clickBalloon);
+            save.addEventHandler(MouseEvent.MOUSE_CLICKED, clickSave);
+
+            HBox.setHgrow(filler, Priority.ALWAYS);
+            HBox controlsContainer = new HBox();
+            controlsContainer.getChildren().addAll(love, balloon, save, filler, delete);
+
+            TextArea tempNote = new TextArea();
+            try {
+                List<String> line = new ArrayList<>();
+                File inFile = new File("D:\\IDEA Project\\Y3\\src\\notes\\" + save.getId() + ".txt");
+                BufferedReader in = new BufferedReader(new FileReader(inFile));
+                String str;
+
+                while((str = in.readLine()) != null) {
+                    line.add(str);
+                }
+
+                for(int j = 0; j < line.size(); j++) {
+                    tempNote.appendText(line.get(j));
+                    if(j != line.size() - 1) {
+                        tempNote.appendText("\n");
+                    }
+                }
+
+                in.close();
+
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+                System.out.println("IOIOIO");
+            }
+
+
+            tempNote.setPromptText("Write your text here!");
+            tempNote.setWrapText(true);
+            tempNote.setStyle(String.valueOf(noteStyle));
+            saveIdList.add(save.getId());
+            taList.add(tempNote);
+            GridPane.setMargin(tempNote, new Insets(0, 0, 10, 0));
+            
+            notePane.add(controlsContainer, gridPaneX, gridPaneY++); // 0 0 -> 1 0 -> 2 0
+            notePane.add(tempNote, gridPaneX++, gridPaneY--); // 0 1 -> 1 1 -> 2 1
+            if(gridPaneX > 2) {
+                gridPaneX = 0;
+                gridPaneY += 2;
+            }
+            curNote+= 1;
+
+            EventHandler<MouseEvent> clickDelete = new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent e) {
+                    System.out.println("CLICK " + delete.getId());
+                    deleteNote(save.getId());
+                    notePane.getChildren().remove(tempNote);
+                    notePane.getChildren().remove(controlsContainer);
+
+                    curNote = 0;
+                    gridPaneX = 0;
+                    gridPaneY = 0;
+
+                    saveIdList.removeAll(saveIdList);
+                    taList.removeAll(taList);
+                    loadNote();
+
+                }
+            };
+            delete.addEventHandler(MouseEvent.MOUSE_CLICKED, clickDelete);
+        }
 
     }
 
-    private void saveNote() {
+    private void saveNote(String saveId) {
+        try {
+            List<String> line = new ArrayList<>();
+            File outFile = new File("D:\\IDEA Project\\Y3\\src\\notes\\" + saveId + ".txt");
+            BufferedWriter out = new BufferedWriter(new FileWriter(outFile));
+            String str;
 
+            for(int i = 0; i < saveIdList.size(); i++) {
+                String tempSaveId = saveIdList.get(i);
+                if(tempSaveId.equals(saveId)) {
+                    TextArea tempTa = taList.get(i);
+                    str = tempTa.getText();
+                    System.out.println(str);
+                    out.write(str);
+                }
+            }
+
+            out.close();
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            System.out.println("IOIOIO");
+        }
     }
 
 
